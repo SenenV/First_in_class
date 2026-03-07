e5540bf46c74f810e1e6a88da84c5caba70c7181
Senen Velasco <senenvelasco@cpp.edu>
2026-03-06 19:25:42 -0800

    inital outline

---
+++ restaurantBill.c
@@ -0,0 +1,24 @@
+/*  
+    Description: 
+
+    Program will be able to calculate the total of a restaurant bill.
+    
+
+
+    Outline:
+
+    1. include stdio.h & stdlib.h.
+
+    2. struct that will contain the meals & prices.
+
+    3. randomly choose the meal.
+
+    4. get tips & taxes from command line.
+
+    5. function that calculates the tax & tips.
+    
+    6. display the total bill.
+
+
+*/
+
