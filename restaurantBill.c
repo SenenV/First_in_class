12036bb164c2b38ad603aa17d15b2461d2998f3a
Senen Velasco <senenvelasco@cpp.edu>
2026-03-06 20:48:01 -0800

    testing total bill func & command line argv

--- restaurantBill.c
+++ restaurantBill.c
@@ -27,7 +27,7 @@
 #include <stdlib.h>
 
 struct menuItems{
-    char rand_meal[50];
+    char item[50];
     float price;
 
 };
@@ -39,9 +39,28 @@ struct menuItems menu[] = {
     {"Pizza", 22.35}
 };
 
-int main(){
+float total_bill(float tax, float tip, float price){
+    float tax_perc = tax / 100;
+    float tip_perc = tip / 100;
+    
+
+    return (price * tax_perc) + (price * tip_perc) + price;
+}
+
+int main(int argc, char *argv[]){
     srand(time(NULL));
     int rand_num = rand() % 4;
+
+    float price = menu[rand_num].price;
+    float tax = atof(argv[1]);
+    float tip = atof(argv[2]);
+
+    float total = total_bill(tax, tip, price);
+
+    printf("random item frm menu: %s, %.2f\n", menu[rand_num].item, menu[rand_num].price);
+    printf("total is %.2f\n", total);
+
+    
 
     
 }
